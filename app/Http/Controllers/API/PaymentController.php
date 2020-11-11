60c6a146ef14d4a4673d91dd3ae8770dbe7c6d4f
unknown <leereal08@ymail.com>
2020-11-11 19:41:43 +0200

    Make Payment Added

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PaymentResource;
+use App\Models\Client;
 use App\Models\Payment;
+use App\Models\Plan;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -29,27 +31,31 @@ class PaymentController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'payment_method_id' =>['required', 'integer'],
-            'client_id'         =>['required', 'integer'],
+            'payment_method'    =>['required', 'integer'],
+            'id'                =>['required', 'integer'],
             'amount'            =>'string|required',
             'ref_number'        =>'string|nullable',
             'month_paid_for'    =>'string|required',  
-            'date_paid'         =>'date|required'                
+            'date_paid'         =>'date|required|before_or_equal:today'                
         ]);
 
+        $time = strtotime('10/16/2003');
+
+$newformat = date('Y-m-d',$time);
+
         $payment                        =   new Payment;
-        $payment->payment_method_id     =   $request->payment_method_id;
-        $payment->client_id             =   $request->client_id;
+        $payment->payment_method_id     =   $request->payment_method;
+        $payment->client_id             =   $request->id;
         $payment->amount                =   $request->amount;
         $payment->description           =   "Payment";
         $payment->receipt_number        =   "RC".Carbon::now()->timestamp;
         $payment->ref_number            =   $request->ref_number;
         $payment->month_paid_for        =   $request->month_paid_for;
-        $payment->date_paid             =   $request->date_paid;
-        $payment->employee_id           =   1;//auth('api')->user()->employee->id;
-        $payment->branch_id             =   1;//auth('api')->user()->branch->id;
-        $payment->plan_id               =   1;//auth('api')->user()->plan->id;
-        $payment->ipAddress             =   request()->ip();
+        $payment->date_paid             =   date('Y-m-d',strtotime($request->date_paid));
+        $payment->employee_id           =   auth('api')->user()->employee->id;
+        $payment->branch_id             =   auth('api')->user()->employee->branch->id;
+        $payment->plan_id               =   Client::where('id',$request->id)->value('plan_id');
+        $payment->ipaddress             =   request()->ip();
 
         if ($payment->save()) {
             return new PaymentResource($payment);
